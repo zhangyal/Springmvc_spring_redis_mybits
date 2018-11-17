69676ef659b92319b13061b24c519b42b439494d
Zhangyaliang01 <374484979@qq.com>
2018-11-17 18:54:41 +0800

    add

---
+++ src/main/java/com/hy/pojo/User.java
@@ -0,0 +1,11 @@
+package com.hy.pojo;
+
+/**
+ * @author pc
+ * @Title: User
+ * @ProjectName Springmvc_spring_redis_mybits
+ * @Description: TODO
+ * @date 2018/11/17-18:52
+ */
+public class User {
+}
